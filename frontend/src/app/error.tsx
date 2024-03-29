89034cf2e6fa7b57e90788560727b81fd8a06968
gioscarda <gioscarda@gmail.com>
2024-03-29 16:06:42 +0100

    Fix glitch on error and not-found pages

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -3,8 +3,8 @@
 import Link from "next/link"
 export default function Error() {
   return (
-    <div className="w-full text-center m-10">
-      <p className="text-semibold text-red-800">Some error occurred! Please try again.</p>
+    <div className="w-[calc(80dvw)] text-center m-10">
+      <p className="text-semibold text-red-800 break-words">Some error occurred! Please try again.</p>
       <p className="mt-10">
           <span>Return  </span>
           <Link href="/" className="text-bold border-2 border-amber-700 rounded-md p-2 bg-amber-200">
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 export default function NotFound() {
   return (
-    <div className="w-full text-center m-10">
-      <p className="text-semibold text-amber-700">The page you are looking for does not exist!</p>
+    <div className="w-[calc(80dvw)] text-center m-10">
+      <p className="text-semibold text-amber-700 break-words">The page you are looking for does not exist!</p>
       <p className="mt-10">
           <span>Return  </span>
           <Link href="/" className="text-bold border-2 border-amber-700 rounded-md p-2 bg-amber-200">
